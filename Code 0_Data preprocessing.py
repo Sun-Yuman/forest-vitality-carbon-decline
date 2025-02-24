22d17aa5af2417980297e01b510409ca306a2117
Sun-Yuman <sunym1994@outlook.com>
2025-02-24 18:57:35 +0800

    Update Code 0_Data preprocessing.py

--- Code 0_Data preprocessing.py
+++ Code 0_Data preprocessing.py
@@ -2,6 +2,8 @@
 This script processes data loading and merging, followed by cleaning, further processing, and saving the cleaned datasets.
 
 1. Load raw data and merge with additional datasets
+ Raw Data(c1_tre.csv): The raw data and additional datasets are available at http://icp-forests.net/page/data-requests.
+ For further inquiries, please contact the corresponding author.
 2. Clean the data by removing rows(code_defoliation) with missing or invalid values
 3: Exclude countries that have fewer than 30 unique 'survey_year' entries
 4. Remove species ID changed across monitoring years and unknown species
@@ -15,8 +17,13 @@ This script processes data loading and merging, followed by cleaning, further pr
 
 import pandas as pd
 import numpy as np
+import os
 # # # # # # # # # # # # # # # # # # Step 1: Load raw data and merge with additional datasets # # # # # # # # # # # # # # # # # #
-data1 = pd.read_csv('C:/Yuman/c1_tre.txt', sep=';', encoding='gbk')
+BASE_DIR = "C:/data"
+
+
+file_path = os.path.join(BASE_DIR, "c1_tre.txt")
+data1 = pd.read_csv(file_path, sep=';', encoding='gbk')
 
 # Print column names and dimensions
 print(data1.columns)
@@ -24,11 +31,16 @@ print(data1.shape[1])  # Number of columns
 print(len(data1))  # Number of rows
 
 # Load additional datasets
-data6 = pd.read_csv('C:/Yuman/biogeo_reg_l109_c2.csv', header=0)
-data7 = pd.read_csv('C:/Yuman/y1_pl1_c2.csv', header=0)
-data8 = pd.read_csv('C:/Yuman/y1_st1_c2.csv', header=0)
-data9 = pd.read_csv('C:/Yuman/d_tree_spec_c2.csv', header=0)
-data10 = pd.read_csv('C:/Yuman/d_country_c2.csv', header=0)
+data6_path = os.path.join(BASE_DIR, "biogeo_reg_l109_c2.csv")
+data7_path = os.path.join(BASE_DIR, "y1_pl1_c2.csv")
+data8_path = os.path.join(BASE_DIR, "y1_st1_c2.csv")
+data9_path = os.path.join(BASE_DIR, "d_tree_spec_c2.csv")
+data10_path = os.path.join(BASE_DIR, "d_country_c2.csv")
+data6 = pd.read_csv(data6_path, encoding='gbk')
+data7 = pd.read_csv(data7_path, encoding='gbk')
+data8 = pd.read_csv(data8_path, encoding='gbk')
+data9 = pd.read_csv(data9_path, encoding='gbk')
+data10 = pd.read_csv(data10_path, encoding='gbk')
 
 # Merge data1 with additional datasets
 data1 = pd.merge(data1, data6[['code_country', 'code_plot', 'biogeo_reg']], on=['code_country', 'code_plot'], how='left')
@@ -43,7 +55,8 @@ data1 = pd.merge(data1, data10[['code_country', 'lib_country', 'code_iso']], on=
 print("Rows after merging with data10:", len(data1))
 
 # Save the merged dataset
-data1.to_csv('C:/Yuman/c1_treconn.txt', sep='\t', encoding='gbk', index=False)
+output_file_path = os.path.join(BASE_DIR, 'c1_treconn.txt')
+data1.to_csv(output_file_path, sep='\t', encoding='gbk', index=False)
 print(data1.shape[1])  # Number of columns
 print(len(data1))  # Number of rows
 
@@ -62,7 +75,8 @@ print(f"Unique combinations of code_country, code_plot, and tree_number: {countr
 
 
 # # # # # # # # # # # # # # # # # # Step 2: Clean the data by removing rows(code_defoliation) with missing or invalid values # # # # # # # # # # # # # # # # # #
-data = pd.read_csv('C:/Yuman/c1_treconn.txt', sep='\t', header=0, encoding='utf-8')
+file_path = os.path.join(BASE_DIR, "c1_treconn.txt")
+data = pd.read_csv(file_path, sep='\t', header=0, encoding='utf-8')
 print(data.shape[1])  # Number of columns
 print(len(data))  # Number of rows
 
@@ -72,7 +86,8 @@ data = data[data['code_defoliation'] != -1]
 data = data[(data['survey_year'] >= 1990) & (data['survey_year'] <= 2022)]
 
 # Save the cleaned data
-data.to_csv('C:/Yuman/c1_tre_valid.txt', sep='\t', encoding='utf-8', index=False)
+output_file_path = os.path.join(BASE_DIR, "c1_tre_valid.txt")
+data.to_csv(output_file_path, sep='\t', encoding='utf-8', index=False)
 
 # Check for missing values in 'biogeo_reg'
 na_count = data['biogeo_reg'].isna().sum()
@@ -93,13 +108,16 @@ print(f"Unique combinations of code_country, code_plot, and tree_number: {countr
 
 
 # # # # # # # # # # # # # # # # # # Step 3: Exclude countries that have fewer than 30 unique 'survey_year' entries # # # # # # # # # # # # # # # # # #
-data = pd.read_csv('C:/Yuman/c1_tre_valid.txt', sep='\t', header=0, encoding='utf-8')
+
+file_path = os.path.join(BASE_DIR, "c1_tre_valid.txt")
+data = pd.read_csv(file_path, sep='\t', header=0, encoding='utf-8')
 grouped_data = data.groupby('code_country')['survey_year'].nunique()
 countries_to_remove = grouped_data[grouped_data < 30].index.tolist()
 data = data[~data['code_country'].isin(countries_to_remove)]
 
 # Save the data after removing specific countries
-data.to_csv('C:/Yuman/c1_tre_valid_19Ctry.txt', sep='\t', encoding='utf-8', index=False)
+output_file_path = os.path.join(BASE_DIR, "c1_tre_valid_19Ctry.txt")
+data.to_csv(output_file_path, sep='\t', encoding='utf-8', index=False)
 print(data['code_country'].unique())  #19
 
 # Check for missing values in 'biogeo_reg'
@@ -121,7 +139,8 @@ print(f"Unique combinations of code_country, code_plot, and tree_number: {countr
 
 
 # # # # # # # # # # # # # # # # # # Step 4: Remove species ID changed across monitoring years and unknown species # # # # # # # # # # # # # # # # # #
-data = pd.read_csv('C:/Yuman/c1_tre_valid_19Ctry.txt', sep='\t', encoding='gbk')
+file_path = os.path.join(BASE_DIR, "c1_tre_valid_19Ctry.txt")
+data = pd.read_csv(file_path, sep='\t', encoding='gbk')
 grouped = data.groupby(['code_country', 'code_plot', 'tree_number'])
 species_counts = grouped['code_tree_species'].nunique().reset_index()
 
@@ -134,7 +153,8 @@ df2 = merged_data[merged_data['code_tree_species_count'] > 1].drop(columns=['cod
 
 # Remove unknown tree species
 delete_unk = df1[df1['code_tree_species'] != 0]
-delete_unk.to_csv('C:/Yuman/allsp_sameDunksp.txt', sep='\t', encoding='gbk', index=False)
+output_file_path = os.path.join(BASE_DIR, "allsp_sameDunksp.txt")
+delete_unk.to_csv(output_file_path, sep='\t', encoding='gbk', index=False)
 
 # Print dataset dimensions after removing unknown species
 print(delete_unk.shape[1])  # Number of columns
@@ -154,11 +174,12 @@ print(f"Unique combinations of code_country, code_plot and tree_number: {country
 
 # # # # # # # # # # # # # # # # # # Step 5: Further split data based on the presence of code_defoliation 100 # # # # # # # # # # # # # # # # # #
 # Retain the values from the beginning to the first occurrence of code_defoliation with a value of 100
-data = pd.read_csv('C:/Yuman/allsp_sameDunksp.txt', sep='\t', encoding='gbk')
+file_path = os.path.join(BASE_DIR, "allsp_sameDunksp.txt")
+data = pd.read_csv(file_path, sep='\t', encoding='gbk')
 
 # Define processing function for each group
 def process_group(group):
-    group.sort_values(by='survey_year', inplace=True)
+    group.sort_values(by='survey_year')
     if 100 in group['code_defoliation'].values:
         first_100_index = np.where(group['code_defoliation'].values == 100)[0][0]
         return group.iloc[:first_100_index + 1], group.iloc[first_100_index + 1:]
@@ -175,7 +196,8 @@ df1 = pd.concat(df1_list)
 df2 = pd.concat(df2_list)
 
 # Save results
-df1.to_csv('C:/Yuman/allspRR.txt', sep='\t', encoding='gbk', index=False)
+output_file_path = os.path.join(BASE_DIR, "allspRR.txt")
+df1.to_csv(output_file_path, sep='\t', encoding='gbk', index=False)
 #df2.to_csv('C:/Yuman/allspDD.csv', encoding='gbk', index=False)
 
 # Print dataset dimensions after splitting
@@ -197,8 +219,8 @@ print(f"Unique combinations of code_country, code_plot and tree_number: {country
 
 
 # # # # # # # # # # # # # # # # # #  Step 6. Clean the dataset by removing specific countries (Broadleaves in Norway and Sweden) # # # # # # # # # # # # # # # # # #
-data = pd.read_csv('C:/Yuman/allspRR.txt', sep='\t', encoding='gbk')
-
+file_path = os.path.join(BASE_DIR, "allspRR.txt")
+data = pd.read_csv(file_path, sep='\t', encoding='gbk')
 # Identify rows to delete
 to_delete = data[(data['code_country'].isin([13, 55])) & (data['grp_tree_species'] == 'broadleaves')]
 
@@ -206,7 +228,8 @@ to_delete = data[(data['code_country'].isin([13, 55])) & (data['grp_tree_species
 data = data[~((data['code_country'].isin([13, 55])) & (data['grp_tree_species'] == 'broadleaves'))]
 
 # Save the cleaned data
-data.to_csv('C:/Yuman/allsp_clean.txt', sep='\t', encoding='gbk', index=False)
+output_file_path = os.path.join(BASE_DIR, "allsp_clean.txt")
+data.to_csv(output_file_path, sep='\t', encoding='gbk', index=False)
 #to_delete.to_csv('C:/Yuman/allspD_clean.csv', encoding='gbk', index=False)
 
 # Print dataset dimensions after cleaning
@@ -228,7 +251,8 @@ print(f"Unique combinations of code_country, code_plot, and tree_number: {countr
 
 
 # # # # # # # # # # # # # # # # # # Step 7: Identify and remove duplicate rows # # # # # # # # # # # # # # # # # #
-data = pd.read_csv('C:/Yuman/allsp_clean.txt', sep='\t', low_memory=False)
+file_path = os.path.join(BASE_DIR, "allsp_clean.txt")
+data = pd.read_csv(file_path, sep='\t', low_memory=False)
 
 # Identify duplicates
 grouped_data1 = data.groupby(['code_country', 'code_plot', 'survey_year', 'tree_number']).size().reset_index(name='count')
@@ -250,13 +274,14 @@ else:
     duplicates_data = pd.DataFrame()
 
 # Save duplicates to a file
-duplicates_data.to_csv('C:/Yuman/Dduplicate_row.csv', index=False)
-
+output_file_path = os.path.join(BASE_DIR, "Dduplicate_row.csv")
+duplicates_data.to_csv(output_file_path, index=False)
 # Remove duplicates and keep the first occurrence
 unique_data = data.drop_duplicates(subset=['code_country', 'code_plot', 'survey_year', 'tree_number'], keep='first')
 
 # Save the unique data
-unique_data.to_csv('C:/Yuman/allsp.txt', sep='\t', index=False)
+output_file_path = os.path.join(BASE_DIR, "allsp.txt")
+unique_data.to_csv(output_file_path, sep='\t', index=False)
 
 # Print dataset dimensions after removing duplicates
 print(unique_data.shape[1])  # Number of columns
@@ -291,13 +316,14 @@ species_mapping = {
 }
 
 # Read the data
-data1 = pd.read_csv('C:/Yuman/allsp.txt', sep='\t', encoding='utf-8')
-
+file_path = os.path.join(BASE_DIR, "allsp.txt")
+data1 = pd.read_csv(file_path, sep='\t', low_memory=False)
 # Assign the 'code_tree_species' to 'English_names'
 data1['English_names'] = data1['code_tree_species'].map(species_mapping)
 
 # Save the mapped data
-data1.to_csv('C:/Yuman/allsp(8).txt', sep='\t', encoding='utf-8', index=False)
+output_file_path = os.path.join(BASE_DIR, "allsp(8).txt")
+data1.to_csv(output_file_path, sep='\t', encoding='utf-8', index=False)
 
 # Print dataset dimensions
 print(data1.shape[1])  # Number of columns
@@ -305,7 +331,7 @@ print(len(data1))  # Number of rows
 na_count = data1['biogeo_reg'].isna().sum()
 print(na_count)
 country_plot_combinations = data1.groupby(['code_country', 'code_plot']).ngroups
-print(f"Unique combinations of code_country and code_number: {country_plot_combinations}")
+print(f"Unique combinations of code_country and code_plot: {country_plot_combinations}")
 
 # Count unique combinations of 'code_country', 'code_plot', and 'tree_number'
 country_plot_tree_combinations = data1.groupby(['code_country', 'code_plot', 'tree_number']).ngroups
@@ -338,7 +364,9 @@ def assign_plotage(value):
 data1['plot_age'] = data1['code_mean_age'].apply(assign_plotage)
 
 # Save the data
-data1.to_csv('C:/Yuman/allsp(8)age.txt', sep='\t', index=False)
+output_file_path = os.path.join(BASE_DIR, "allsp(8)age.txt")
+
+data1.to_csv(output_file_path, sep='\t', encoding='utf-8', index=False)
 
 # Print dataset dimensions after processing
 print(data1.shape[1])  # Number of columns
@@ -355,7 +383,7 @@ print(f"Unique combinations of code_country, code_plot and tree_number: {country
 
 
 # # # # # # # # # # # # # # # # # # Step 10: Remain specific columns from the dataset while discarding unnecessary ones # # # # # # # # # # # # # # # # # #
-file_path = 'C:/Yuman/allsp(8)age.txt'
+file_path = os.path.join(BASE_DIR, "allsp(8)age.txt")
 data = pd.read_csv(file_path, sep='\t')
 
 # Define the list of columns to keep
@@ -376,5 +404,6 @@ print(data_filtered.columns.tolist())
 print("\nTotal number of rows after filtering:")  #2688512
 print(len(data_filtered))
 # Save the processed data
-data_filtered.to_csv('C:/Yuman/allsp(8)age.txt', sep='\t', index=False)
-
+output_file_path = os.path.join(BASE_DIR, "allsp(8)age.txt")
+data_filtered.to_csv(output_file_path, sep='\t', index=False)
+#Defoliation_xydm.txt include allsp(8)age.txt and tree damage data.
